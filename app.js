c250c7cc3ec9007e1ad33aa1ac296d4f4d10305a
ssentamuabel <ssentamuabel90@gmail.com>
2023-10-23 04:06:52 -0700

    First commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const morgan = require('morgan')
+const createError = require('http-errors')
+require('dotenv').config()
+const AuthRoute = require('./Routes/Auth.route')
+
+
+const app = express()
+
+app.get('/', async(req, res, next) =>{
+    res.send("Hello from Express. ")
+})
+
+
+
+app.use('/auth', AuthRoute )
+
+
+app.use(async(req, res, next) =>{
+    // const error = new Error("Not found")
+    // error.status = 404
+    // next(error)
+    next(createError.NotFound())
+})
+
+
+
+app.use((error, req, res, next) =>{
+    res.status(error.status || 500)
+    res.send({
+        error:{
+            status:error.status || 500,
+            message:error.message,
+        },
+    })
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, ()=>{
+     console.log(`Server running on port ${PORT}`)
+})
